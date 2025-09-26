3eb791ce4add5e92559a5420a42b70aafc277f78
Nghia Pham <minidora2707@gmail.com>
2025-09-26 16:30:49 +0700

    first commit

---
+++ src/main/java/prm/project/prm392backend/Prm392BackendApplication.java
@@ -0,0 +1,13 @@
+package prm.project.prm392backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prm392BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prm392BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prm/project/prm392backend/Prm392BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package prm.project.prm392backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prm392BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
