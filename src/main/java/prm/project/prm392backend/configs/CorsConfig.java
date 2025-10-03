434e9c48facca44ab825761d8f2daf763560fc87
Nghia Pham <minidora2707@gmail.com>
2025-10-03 16:54:52 +0700

    cors

---
+++ src/main/java/prm/project/prm392backend/configs/CorsConfig.java
@@ -0,0 +1,26 @@
+package prm.project.prm392backend.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:3000") // Adjust the allowed origin as needed
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+
+    }
+}
+
